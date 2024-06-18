f9bc9f669fa7d4423d65d8c486d1018514b43140
Your Name <you@example.com>
2024-06-19 00:21:25 +0400

    addedLinkPointBlackLogo.png file

--- src/LinkPoint.MVC/obj/Debug/net8.0/LinkPoint.MVC.AssemblyInfo.cs
+++ src/LinkPoint.MVC/obj/Debug/net8.0/LinkPoint.MVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LinkPoint.MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5443fb30e2bec6c52c4beb345e6fdb3683e14fa5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0b5f566b6a3dbe2d581ebab661e20899a1dd9d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("LinkPoint.MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LinkPoint.MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
